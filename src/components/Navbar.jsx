2a83e0061793c6e090a4eafbe59d64a1cea24a0d
evandaru <fznhsym@gmail.com>
2024-08-27 06:34:57 +0700

    fix: footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
-        <nav className="flex items-center justify-between mb-40">
-            <img src="assets/image/navbar-logo.png" alt="Logo" />
+        <nav className="flex items-center justify-between mb-40 ">
+            <img className="w-36" src="assets/image/logo-programer.svg" alt="Logo" />
             <a target='_blank' href="https://blog.evandaru.site">
                 <button className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500">
                     My Blog
